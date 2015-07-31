3856bde0a0c5909ad324599aba0e1237aae93cb9
HH-KIM <h2k9161@gmail.com>
2015-07-31 16:18:27 +0900

    Create FirstFactorial.rb

---
+++ FirstFactorial.rb
@@ -0,0 +1,12 @@
+def FirstFactorial(num)
+
+  # code goes here
+  num = (1..8).inject(:*)
+  
+  return num
+         
+end
+   
+# keep this function call here 
+# to see how to enter arguments in Ruby scroll down   
+FirstFactorial(STDIN.gets)           
